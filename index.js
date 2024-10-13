8d90d7c0289d5c0fff85cffebe5ac88901316066
Singh <sabhisheksignh343104@gmail.com>
2024-10-14 01:49:04 +0530

    images and videos are removed

--- index.js
+++ index.js
@@ -9,18 +9,23 @@ const home = fs.readFileSync("index.html");
 
 const server = http.createServer((req, res)=>{
     console.log(req.url);
-    if(req.url==='/'){
-        return res.end(home);
+    try {
+        if(req.url==='/'){
+            return res.end(home);
+        }
+        else if(req.url==='/about'){
+            return res.end("<h1>About - Coming soon...</h1>");
+        }
+        else if(req.url==='/contact'){
+            return res.end("<h1>Contact - Coming soon...</h1>");
+        }
+        else{
+            return res.end("<h1 style='color: red;'>404 Page Not Found</h1>");
+        }
+    } catch (error) {
+        return res.end(error.message);
     }
-    else if(req.url==='/about'){
-        return res.end("<h1>About - Coming soon...</h1>");
-    }
-    else if(req.url==='/contact'){
-        return res.end("<h1>Contact - Coming soon...</h1>");
-    }
-    else{
-        return res.end("<h1 style='color: red;'>404 Page Not Found</h1>");
-    }
+    
 });
 
 server.listen(port, ()=>{
